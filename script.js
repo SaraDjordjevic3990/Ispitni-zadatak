2cf7a4e9d8fbcbfcfc88ba03e0831f72abc9a10b
SaraDjordjevic3990 <93790054+SaraDjordjevic3990@users.noreply.github.com>
2022-02-02 01:52:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".teachers-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
